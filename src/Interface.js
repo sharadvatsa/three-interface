e26abe55456718fdb1dd6c91bd19b65900bc997c
Ash Connell <contact@ashconnell.com>
2016-03-15 15:48:32 +1100

    Update 2016-03-15T04:47:50.275Z

--- src/Interface.js
+++ src/Interface.js
@@ -294,8 +294,11 @@ THREE.Interface.prototype.performClick = function (point) {
         if (click.y >= btn.top && click.y <= btn.bottom &&
             click.x >= btn.left && click.x <= btn.right) {
 
-            var method = this.methods[btn.method];
-            if (method) this.methods[btn.method](btn.elem);
+            eval('this.methods.' + btn.onclick);
+
+            // var method = this.methods[btn.onclick];
+            // var value = btn.value;
+            // if (method) this.methods(value);
         }
     }
 
@@ -307,7 +310,7 @@ THREE.Interface.prototype.getBounds = function (clone) {
 
     this.bounds = [];
 
-    var elems = clone.querySelectorAll('[method]');
+    var elems = clone.querySelectorAll('[onclick]');
 
     for (var i=0; i < elems.length; i++) {
         var btn = elems[i];
@@ -320,7 +323,7 @@ THREE.Interface.prototype.getBounds = function (clone) {
             bottom: b.bottom,
             width: b.width,
             height: b.height,
-            method: btn.getAttribute('method'),
+            onclick: btn.getAttribute('onclick'),
             elem: btn
         });
     }
