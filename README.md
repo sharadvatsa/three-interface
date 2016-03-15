03dba9817143dd5d87a8240021f6ca347eef471e
Ash Connell <contact@ashconnell.com>
2016-03-15 13:57:37 +1100

    Update 2016-03-15T02:56:50.422Z

--- README.md
+++ README.md
@@ -1,7 +1,40 @@
 # ThreeJS Interface
 
-Fully interactive HTML and CSS that works with WebVR
+A [ThreeJS][three] extension that lets you place interactive HTML and CSS into your scene. This is most useful for WebVR 
+as it works with stereoscopic rendering.
 
-Basic Example: http://envokevr.github.io/three-interface/
+## Examples
 
-Textbox Example: http://envokevr.github.io/three-interface/textbox.html
+[Basic Example](http://envokevr.github.io/three-interface/)
+
+[Textbox and Virtual Keyboard](http://envokevr.github.io/three-interface/textbox.html)
+
+## Usage
+
+```javascript
+    var html = '<div method="click">Click Me</div>';
+    var methods = {
+        click: function (elem) {
+            console.log('element clicked!', elem);
+        }
+    };
+    var options = {
+        throttle: 250          // throttle for the renderer in milliseconds, can be disabled with false (default 250ms)
+        observe: true,         // watches the element for changes and re-renders (default true)
+        alwaysOnTop: false,    // ensures the UI is always on top of everything in the scene (default false) 
+        debug: false           // places a small sphere at the click point (default false)
+    };
+    var ui = new THREE.Interface(html, methods, options);
+    scene.add(ui);
+```
+
+### Arguments
+
+| Param | Type | Details |
+| ----- | ---- | ------- |
+| html  | `HTMLElement`, `String` or `Array` | The html to be placed into your scene
+| methods | `Object` | a dictionary of interaction methods
+| options | `Object` | a dictionary of options
+
+[three]: http://threejs.org/
+[css3d]: http://threejs.org/
--- src/Interface.js
+++ src/Interface.js
@@ -3,6 +3,7 @@
  */
 
 var html2canvas = require('html2canvas');
+var throttle = require('lodash.throttle');
 
 THREE.Interface = function (html, methods, options) {
 
@@ -12,6 +13,14 @@ THREE.Interface = function (html, methods, options) {
     this.methods = methods || {};
     this.options = options || {};
     this.scaler = 0.0025;
+    this.queue = [];
+
+    // set throttling
+
+    if (this.options.throttle !== false) {
+        this.options.throttle = this.options.throttle || 250;
+        this.render = throttle(this.render, this.options.throttle);
+    }
 
     // bind method contexts to this instance
 
@@ -21,19 +30,12 @@ THREE.Interface = function (html, methods, options) {
         }
     }
 
-    // initialise our texture and canvas image loading logic
+    // create singleton image we will update with canvas renders
 
-    this.texture = new THREE.Texture();
     this.image = new Image();
+    this.image.onload = this.onImageReady.bind(this);
 
-    this.image.onload = function () {
-
-        this.texture.image = this.image;
-        this.texture.needsUpdate = true;
-
-    }.bind(this);
-
-    // render the html onto the plane
+    // render the interface plane
 
     this.render(html);
 }
@@ -41,28 +43,137 @@ THREE.Interface = function (html, methods, options) {
 THREE.Interface.prototype = Object.create( THREE.Object3D.prototype );
 THREE.Interface.prototype.constructor = THREE.Interface;
 
-THREE.Interface.prototype.build = function (html) {
+THREE.Interface.prototype.render = function (html) {
 
-    // decipher and turn html in a node
+    // convert html to element
 
-    this.element = this.makeElement(html);
-
-    // remove existing observer (if any)
-
-    if (this.observer) {
-        this.observer.disconnect();
+    if (html) {
+        this.element = this.makeElement(html);
     }
 
-    // set up observer if option is set
+    // clone the elment and add to dom
 
-    if (this.options.observe === true) {
+    var clone = this.element.cloneNode(true);
 
-        var config = { attributes: true, childList: true, characterData: true, subtree: true };
-        this.observer = new MutationObserver(this.onChange.bind(this));
-        this.observer.observe(this.element, config);
+    this.queue.push(clone);
+
+    if (!this.isRendering) {
+        this.renderNext();
+    }
+
+}
+
+THREE.Interface.prototype.renderImmediate = function (html) {
+
+}
+
+THREE.Interface.prototype.renderNext = function () {
+
+    this.isRendering = true;
+
+    var clone = this.queue.shift();
+
+    document.body.appendChild(clone);
+
+    // load any <img> tags in the dom element
+
+    this.loadImages(clone, function () {
+
+        // check size changes
+
+        this.sizeChanged = (this.lastHeight !== clone.clientHeight || this.lastWidth !== clone.clientWidth);
+        this.lastHeight = clone.clientHeight;
+        this.lastWidth = clone.clientWidth;
+
+        // render!
+
+        html2canvas(clone).then(function (canvas) {
+
+            // update image src, will fire loaded event when ready
+
+            this.image.src = canvas.toDataURL();
+
+            // calculate button positions
+
+            this.getBounds(clone);
+
+            // dispose of cloned element
+
+            document.body.removeChild(clone);
+            clone = null;
+
+        }.bind(this));
+
+    }.bind(this));
+
+}
+
+THREE.Interface.prototype.onImageReady = function () {
+
+    // create or recreate plane if size changed
+
+    if (this.sizeChanged) {
+        this.makePlane();
+    }
+
+    // dispose of any previous texture
+
+    if (this.plane.material.map) {
+        this.plane.material.map.dispose();
+    }
+
+    // update plane texture
+
+    this.plane.material.map = new THREE.Texture(this.image);
+    this.plane.material.map.needsUpdate = true;
+
+    if (this.queue.length) {
+        this.renderNext();
+    } else {
+        this.isRendering = false;
+    }
+
+}
+
+THREE.Interface.prototype.makePlane = function () {
+
+    // remove and dispose of current plane (if any)
+
+    if (this.plane) {
+
+        this.remove(this.plane);
+        this.plane.geometry.dispose();
+        this.plane.material.dispose();
 
     }
 
+    // transpose element to plane size using scaler
+
+    var width = this.scaler * this.lastWidth;
+    var height = this.scaler * this.lastHeight;
+
+    // create plane
+
+    // var texture = new THREE.Texture(this.image);
+    var geometry = new THREE.PlaneGeometry(width, height);
+    var material = new THREE.MeshLambertMaterial({
+
+        // texture will be updated with canvas render
+
+        // map: texture,
+
+        // enable css opacity, rgba etc
+
+        transparent: true,
+
+        // makes the interface always on top
+
+        depthTest: !this.options.alwaysOnTop
+
+    });
+
+    this.plane = new THREE.Mesh(geometry, material);
+    this.add(this.plane);
 }
 
 THREE.Interface.prototype.makeElement = function (html) {
@@ -103,133 +214,29 @@ THREE.Interface.prototype.makeElement = function (html) {
         console.error('html must be String, Array or HTMLElement');
     }
 
+    // remove existing observer (if any)
+
+    if (this.observer) {
+        this.observer.disconnect();
+    }
+
+    // set up observer if option is set
+
+    if (this.options.observe === true) {
+
+        var config = { attributes: true, childList: true, characterData: true, subtree: true };
+        this.observer = new MutationObserver(this.onElementChange.bind(this));
+        this.observer.observe(elem, config);
+
+    }
+
     return elem;
+
 }
 
-THREE.Interface.prototype.onChange = function () {
-
-    // fired when observe notices change - render again
+THREE.Interface.prototype.onElementChange = function () {
 
     this.render();
-
-}
-
-THREE.Interface.prototype.updatePlane = function (clone) {
-
-    // transpose element to plane size using scaler
-
-    var width = this.scaler * clone.clientWidth;
-    var height = this.scaler * clone.clientHeight;
-
-    // remove any existing plane
-
-    if (this.plane) this.remove(this.plane);
-
-    // create plane
-
-    var geometry = new THREE.PlaneGeometry(width, height);
-    var material = new THREE.MeshLambertMaterial({
-
-        // texture will be updated with canvas render
-
-        map: this.texture,
-
-        // enable css opacity, rgba etc
-
-        transparent: true,
-
-        // makes the interface always on top
-
-        depthTest: !this.options.alwaysOnTop
-
-    });
-
-    this.plane = new THREE.Mesh(geometry, material);
-    this.add(this.plane);
-}
-
-THREE.Interface.prototype.performClick = function (point) {
-
-    // convert ray point from world to local
-
-    this.plane.worldToLocal(point);
-
-    // debug shows click point
-
-    if (this.options.debug) {
-        var geometry = new THREE.SphereGeometry(0.05);
-        var material = new THREE.MeshLambertMaterial({ color: 'green' });
-        var box = new THREE.Mesh(geometry, material);
-        box.position.copy(point);
-        this.plane.add(box);
-    }
-
-    // convert point into pixel coords
-
-    var click = {
-        x: (point.x + this.data.plane.halfWidth) / this.scaler,
-        y: -(point.y - this.data.plane.halfHeight) / this.scaler
-    };
-
-    // fire any hits (if any)
-
-    for (var i=0; i < this.data.buttons.length; i++) {
-        var btn = this.data.buttons[i];
-
-        if (click.y >= btn.top && click.y <= btn.bottom &&
-            click.x >= btn.left && click.x <= btn.right) {
-
-            var method = this.methods[btn.method];
-            if (method) this.methods[btn.method](btn.elem);
-        }
-    }
-
-}
-
-THREE.Interface.prototype.render = function (html) {
-
-    // build the passed in html and set up observers
-
-    if (html) this.build(html);
-
-    // clone the elment and add to dom
-
-    var clone = this.element.cloneNode(true);
-    document.body.appendChild(clone);
-
-    this.image.src = '';
-    if (this.options.clearTexture !== false) {
-        this.texture.image = null;
-        this.texture.needsUpdate = true;
-    }
-
-    // wait for images to load
-
-    this.loadImages(clone, function () {
-
-        // build or rebuild the plane with the correct size
-
-        this.updatePlane(clone);
-
-        // calculate button positions
-
-        this.getBounds(clone);
-
-        // render it
-
-        html2canvas(clone).then(function (canvas) {
-
-            // set the image src which will update the texture when it loads
-
-            this.image.src = canvas.toDataURL();
-
-            // remove from dom (performance?)
-
-            document.body.removeChild(clone);
-
-        }.bind(this));
-
-    }.bind(this));
 
 }
 
@@ -254,30 +261,59 @@ THREE.Interface.prototype.loadImages = function (clone, done) {
         });
     }
 
+}
+
+THREE.Interface.prototype.performClick = function (point) {
+
+    // convert ray point from world to local
+
+    this.plane.worldToLocal(point);
+
+    // debug shows click point
+
+    if (this.options.debug) {
+        var geometry = new THREE.SphereGeometry(0.05);
+        var material = new THREE.MeshLambertMaterial({ color: 'green' });
+        var box = new THREE.Mesh(geometry, material);
+        box.position.copy(point);
+        this.plane.add(box);
+    }
+
+    // convert point into pixel coords
+
+    var click = {
+        x: (point.x + (this.plane.geometry.parameters.width / 2)) / this.scaler,
+        y: -(point.y - (this.plane.geometry.parameters.height / 2)) / this.scaler
+    };
+
+    // fire any hits (if any)
+
+    for (var i=0; i < this.bounds.length; i++) {
+        var btn = this.bounds[i];
+
+        if (click.y >= btn.top && click.y <= btn.bottom &&
+            click.x >= btn.left && click.x <= btn.right) {
+
+            var method = this.methods[btn.method];
+            if (method) this.methods[btn.method](btn.elem);
+        }
+    }
 
 }
 
 THREE.Interface.prototype.getBounds = function (clone) {
-    this.data = {};
-
-    // calculate plane bounds etc
-
-    this.data.plane = {
-        halfWidth: this.plane.geometry.parameters.width / 2,
-        halfHeight: this.plane.geometry.parameters.height / 2
-    };
 
     // get button bounds
 
-    var elems = clone.querySelectorAll('[method]');
+    this.bounds = [];
 
-    this.data.buttons = [];
+    var elems = clone.querySelectorAll('[method]');
 
     for (var i=0; i < elems.length; i++) {
         var btn = elems[i];
         var b = btn.getBoundingClientRect();
 
-        this.data.buttons.push({
+        this.bounds.push({
             top: b.top,
             left: b.left,
             right: b.right,
